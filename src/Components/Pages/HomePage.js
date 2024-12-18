ddbc9769df38047fdf67f784e0bc6ef3de8b1840
kybol <kimberley.511@gmail.com>
2024-12-18 15:18:38 +0100

    installed tailwind

---
+++ src/Components/Pages/HomePage.js
@@ -0,0 +1,10 @@
+const HomePage = () => {
+  const main = document.querySelector('main');
+  main.innerHTML = `
+   <h1 className="text-3xl font-bold underline">
+      Hello world!
+  </h1>
+    `;
+};
+
+export default HomePage;
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      'white': '#ffffff',
+      'grey':{
+        100: '#999999',
+        800: '#1C1B1F'
+      },
+      'black': '#000000'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
